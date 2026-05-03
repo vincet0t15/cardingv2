c047bbad246ea5720a4d9cae4417923677e84a05
vincet0t15 <vincet0t15@gmail.com>
2026-05-03 13:55:32 +0800

    aa

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,6 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     <link rel="icon" type="image/png" href="/LGU_logo.png">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.svg">
-    <link rel="manifest" href="/manifest.webmanifest">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
@@ -21,13 +19,6 @@
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
     @inertiaHead
 
-    <script>
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js').catch(() => {});
-            });
-        }
-    </script>
 </head>
 
 <body class="font-sans antialiased">
